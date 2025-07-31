5cf7e6cff5f94a2bfc71596d9f79a60bb3342015
moshaosama <mohamedOSFekry@gmail.com>
2025-07-31 19:34:10 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import dotenv from "dotenv";
+import { handleSocket } from "./SocketHandler/Socket";
+import { connectDB } from "./ConnectDB/DB";
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
@@ -11,24 +13,9 @@ const io = new Server(server, {
 });
 dotenv.config();
 
-let Sockets = [];
-
-io.on("connection", (socket) => {
-  console.log("Connecting...");
-  let socketsArr = [];
-  socketsArr.push(socket.id);
-
-  socket.on("Send-Message", (data) => {
-    socket.broadcast.emit("res-Message", data);
-  });
-
-  const user = { id: socket.id, userName: "Mosha" };
-
-  Sockets.push(user);
-
-  io.emit("get-sockets", Sockets);
-});
+handleSocket(io);
 
 server.listen(process.env.PORT, () => {
   console.log(`Listening on Port ${process.env.PORT}`);
+  connectDB();
 });
