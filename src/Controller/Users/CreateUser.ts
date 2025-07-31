21377a08284c568eeebab64b0fb56bb8a5319449
moshaosama <mohamedOSFekry@gmail.com>
2025-07-31 19:34:15 +0300

    Add Controller folder

---
+++ src/Controller/Users/CreateUser.ts
@@ -0,0 +1,10 @@
+import { DB } from "../../ConnectDB/DB";
+
+export const Createuser = async (user: { id: string; userName: string }) => {
+  const QueryCreateuser =
+    "INSERT INTO users (user_id , userName) VALUES (? , ?)";
+
+  const ValueCreateUser = [user.id, user.userName];
+
+  await DB.promise().query(QueryCreateuser, ValueCreateUser);
+};
